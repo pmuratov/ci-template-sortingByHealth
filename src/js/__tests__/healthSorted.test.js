eb40c7fe3891ced64adf8426f880d284e5ed166d
iPadla <petrozavr@ya.ru>
2025-01-05 18:27:52 +0300

    doesnt work

--- src/js/__tests__/healthSorted.test.js
+++ src/js/__tests__/healthSorted.test.js
@@ -1,27 +1,29 @@
 import sorter from "../sortByHealthList.js";
 
-let charsList = [
-  { name: "мечник", health: 10 },
-  { name: "маг", health: 100 },
-  { name: "бард", health: 80 },
-  { name: "варвар", health: 40 },
-  { name: "рыцарь", health: 90 },
-  { name: "монах", health: 60 },
-  { name: "тихоходка", health: 79 },
-  { name: "покемон", health: 99 },
-  { name: "голый землекоп", health: 9 },
-];
+test("should sort by health, descending", () => {
+  let charsList = [
+    { name: "мечник", health: 10 },
+    { name: "маг", health: 100 },
+    { name: "бард", health: 80 },
+    { name: "варвар", health: 40 },
+    { name: "рыцарь", health: 90 },
+    { name: "монах", health: 60 },
+    { name: "тихоходка", health: 79 },
+    { name: "покемон", health: 99 },
+    { name: "голый землекоп", health: 9 },
+  ];
 
-let expected = [
-  { name: "маг", health: 100 },
-  { name: "покемон", health: 99 },
-  { name: "рыцарь", health: 90 },
-  { name: "бард", health: 80 },
-  { name: "тихоходка", health: 79 },
-  { name: "монах", health: 60 },
-  { name: "варвар", health: 40 },
-  { name: "мечник", health: 10 },
-  { name: "голый землекоп", health: 9 },
-];
+  let expected = [
+    { name: "маг", health: 100 },
+    { name: "покемон", health: 99 },
+    { name: "рыцарь", health: 90 },
+    { name: "бард", health: 80 },
+    { name: "тихоходка", health: 79 },
+    { name: "монах", health: 60 },
+    { name: "варвар", health: 40 },
+    { name: "мечник", health: 10 },
+    { name: "голый землекоп", health: 9 },
+  ];
 
-expect(expected).toEqual(charsList.sort(sorter));
+  expect(expected).toEqual(charsList.sort(sorter));
+});
