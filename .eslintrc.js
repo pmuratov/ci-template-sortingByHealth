f755d5ab795a1fb193c0f633e0b09565987a67ff
iPadla <petrozavr@ya.ru>
2025-01-06 00:21:38 +0300

    remove unused files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,23 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
-    'node': true
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
   },
-  'extends': 'eslint:recommended',
-  'overrides': [
+  extends: "eslint:recommended",
+  overrides: [
     {
-      'files': ['**/*/*.test.js'],
-      'plugins': ['jest'],
-      'extends': ['plugin:jest/recommended'],
-      'rules': { 'jest/prefer-expect-assertions': 'off' }
-    }
+      files: ["**/*/*.test.js"],
+      plugins: ["jest"],
+      extends: ["plugin:jest/recommended"],
+      rules: { "jest/prefer-expect-assertions": "off" },
+    },
   ],
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module'
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  'rules': {}
-}
+  rules: {
+    "no-unused-vars": "off",
+  },
+};
